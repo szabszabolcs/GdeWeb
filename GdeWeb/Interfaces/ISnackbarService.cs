b9b6f4bdc252eb96f19745d94debb4459308c5ae
szabszabolcs <64633160+szabszabolcs@users.noreply.github.com>
2025-12-19 20:05:41 +0100

    Revert "alap"
    This reverts commit 11c3112a980666a4d7613e1e8914d6034d2d57ff.

--- GdeWeb/Interfaces/ISnackbarService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MudBlazor;
-
-namespace GdeWeb.Interfaces
-{
-    public interface ISnackbarService
-    {
-        public void ShowSnackbar(Severity severity, string message, int pageWidth = 700, bool clearAfterNavigation = true);
-    }
-}
---
+++ GdeWebLA09Models/LoginTokenModel.cs
@@ -0,0 +1,11 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace GdeWebLA09Models
+{
+    [SwaggerSchema("Login token osztálya")]
+    public class LoginTokenModel
+    {
+        [SwaggerSchema("Login token azonosítója")]
+        public string Token { get; set; } = String.Empty;
+    }
+}
